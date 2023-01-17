689179c0747112d7a78a4b5d862040aa5fd70940
André Alves Correia <58227057+andrealvescorreia@users.noreply.github.com>
2023-01-17 17:37:22 -0300

    frontend setup

---
+++ web/tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.tsx', // diz que todos os arquivos tsx terão estilização
+    './index.html' // e o index.html tbm
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
