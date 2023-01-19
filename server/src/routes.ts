a5ef7ed53123fc0ac5bbe008a92248b885fc2b31
André Alves Correia <58227057+andrealvescorreia@users.noreply.github.com>
2023-01-19 20:19:30 -0300

    summary route

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -148,16 +148,19 @@ export async function appRoutes(app: FastifyInstance){
                     FROM day_habits DH
                     WHERE DH.day_id = D.id
                 ) as completed
+                ,
                 (
                     SELECT
                         cast(count(*) as float)
                     FROM habit_week_days HWD
+                    JOIN habits H
+                        ON H.id = HWD.habit_id
                     WHERE 
-                        HWd.week_day = cast(strftime('%w', D.date/1000.0, 'unixepoch')) as int
+                        HWD.week_day = cast(strftime('%w', D.date/1000.0, 'unixepoch') as int) 
+                        AND H.created_at <= D.date
                 ) as amount
             FROM days D
-
-
         `
+        return summary
     })
 }
