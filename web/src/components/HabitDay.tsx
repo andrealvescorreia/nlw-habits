6efff8b9cf4efe9eeb65555e7c6a6a4a3204a3dc
André Alves Correia <andre2013aguabranca@gmail.com>
2023-01-22 17:19:10 -0300

    Habit toggle working
    patch request working

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,57 +1,53 @@
 import * as Popover from '@radix-ui/react-popover';
-import * as Checkbox from '@radix-ui/react-checkbox';
 import clsx from 'clsx';
 import { ProgressBar } from './ProgressBar';
-import { Check } from 'phosphor-react';
-import { CheckBox } from './CheckBox';
 import dayjs from 'dayjs';
+import { HabitsList } from './HabitsList';
 
 // Properties
-interface HabitProps{
-    date: Date
-    amount?: number // total de habitos disponiveis para um dado dia
-    completed?: number // quantos desses foram completos
+interface HabitProps {
+   date: Date
+   amount?: number // total de habitos disponiveis para um dado dia
+   completed?: number // quantos desses foram completos
 }
 
 // PRECISA começar com Maiusculo!
-export function HabitDay({completed = 0, amount = 0, date}: HabitProps){
-   const completedPercentage = amount > 0 ? Math.round( (completed / amount) * 100) : 0
-   
+export function HabitDay({ completed = 0, amount = 0, date }: HabitProps) {
+   const completedPercentage = amount > 0 ? Math.round((completed / amount) * 100) : 0
+
    const dayAndMonth = dayjs(date).format('DD/MM')
    const dayOfWeek = dayjs(date).format('dddd') // ex: 'terça-feira'
 
    const today = dayjs().startOf('day').toDate()
    const isCurrentDay = dayjs(date).isSame(today)
 
-    return (
-        <Popover.Root>
-            <Popover.Trigger 
+   return (
+      <Popover.Root>
+         <Popover.Trigger
             className={clsx('w-10 h-10 border-2 rounded-lg', {
-                ' bg-zinc-900  border-zinc-800': completedPercentage === 0,
-                ' bg-violet-900  border-violet-700': completedPercentage > 0 && completedPercentage < 20,
-                ' bg-violet-800  border-violet-600': completedPercentage >= 20 && completedPercentage < 40,
-                ' bg-violet-700  border-violet-500': completedPercentage >= 40 && completedPercentage < 60,
-                ' bg-violet-600  border-violet-500': completedPercentage >= 60 && completedPercentage < 80,
-                ' bg-violet-500  border-violet-400': completedPercentage >= 80,
-                'border-white border-4': isCurrentDay
+               ' bg-zinc-900  border-zinc-800': completedPercentage === 0,
+               ' bg-violet-900  border-violet-700': completedPercentage > 0 && completedPercentage < 20,
+               ' bg-violet-800  border-violet-600': completedPercentage >= 20 && completedPercentage < 40,
+               ' bg-violet-700  border-violet-500': completedPercentage >= 40 && completedPercentage < 60,
+               ' bg-violet-600  border-violet-500': completedPercentage >= 60 && completedPercentage < 80,
+               ' bg-violet-500  border-violet-400': completedPercentage >= 80,
+               'border-white border-4': isCurrentDay
             })}
-            />
-            
-            <Popover.Portal>
-                <Popover.Content className='min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col'>
-                    <Popover.Arrow height={8} width={16} className="fill-zinc-900"/>
-                
-                    <span className='font-semibold text-zinc-400'>{dayOfWeek}</span>
-                    <span className='font-extrabold mt-1 text-3xl leading-tight'>{dayAndMonth}</span>
-                    <ProgressBar progress={completedPercentage}/>
+         />
 
-                    <CheckBox />
-                    <CheckBox />
-                    <CheckBox />
+         <Popover.Portal>
+            <Popover.Content className='min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col'>
+               <Popover.Arrow height={8} width={16} className="fill-zinc-900" />
 
-                </Popover.Content>
-            </Popover.Portal>
-        </Popover.Root>
+               <span className='font-semibold text-zinc-400'>{dayOfWeek}</span>
+               <span className='font-extrabold mt-1 text-3xl leading-tight'>{dayAndMonth}</span>
+               <ProgressBar progress={completedPercentage} />
 
-    )
+               <HabitsList date={date}/>
+
+            </Popover.Content>
+         </Popover.Portal>
+      </Popover.Root>
+
+   )
 }
---
+++ web/src/components/HabitsList.tsx
@@ -0,0 +1,94 @@
+import * as Checkbox from "@radix-ui/react-checkbox";
+import dayjs from "dayjs";
+import { Check } from "phosphor-react";
+import { useEffect, useState } from "react";
+import { api } from "../lib/axios";
+
+
+interface HabitsListProps {
+   date: Date
+}
+
+interface HabitsInfo {
+   possibleHabits: Array<
+      { id: string; title: string; createdAt: string }
+   >
+   completedHabits: string[]
+}
+
+export function HabitsList({ date }: HabitsListProps) {
+
+   const [habitsInfo, setHabitsInfo] = useState<HabitsInfo>()
+
+   useEffect(() => {
+      api.get('day', {
+         params: {
+            date: date.toISOString()
+         }
+      }).then(response => {
+         setHabitsInfo(response.data)
+      })
+
+   }, [])
+
+   const isDateInPast = dayjs(date)
+                       .endOf('day')
+                       .isBefore(new Date())
+
+   async function handleToggleHabit(habitId: string){
+      await api.patch(`habits/${habitId}/toggle`)
+      const isHabitAlreadyComplete = habitsInfo!.completedHabits.includes(habitId)
+      
+      let completedHabits: string[] = []
+
+      if(isHabitAlreadyComplete){
+         // desmarca
+         completedHabits = habitsInfo!.completedHabits.filter(id => id !== habitId)// remove da lista
+         
+      }else{
+         // marca
+         completedHabits = [...habitsInfo!.completedHabits, habitId]// adiciona a lista
+      }
+      // atualiza o estado (para mostrar vizualmente)
+      setHabitsInfo({
+         possibleHabits: habitsInfo!.possibleHabits,
+         completedHabits
+      })
+      
+   }
+
+   return (
+      <div className="mt-6 flex flex-col gap-3">
+         {habitsInfo?.possibleHabits.map(habit => {
+            return (
+               <Checkbox.Root
+                  key={habit.id}
+                  checked={habitsInfo.completedHabits.includes(habit.id)}// deixa o habito vizualmente marcado, caso foi concluido
+                  onCheckedChange={() => handleToggleHabit(habit.id)}
+                  disabled={isDateInPast}// impossibilita marcar/desmarcar habitos de dias que ja passaram.
+                  className='flex items-center gap-3 group'
+                  // No tailwind, o 'group' faz com que todos os elementos dentro de outro (Checkbox.Root) possam herdar atributos de estilização. 
+                  // Nesse caso queremos o 'data-state' em específico, pois ele indica se o checkbox está "checked" ou "unchecked".
+               >
+                  <div /* quadradinho do checkbox*/
+                     className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 
+                             group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'//quando "checked", fundo verde
+                  >
+                     <Checkbox.Indicator /* (marcado)*/ >
+                        <Check size={20} className="text-white" />
+                     </Checkbox.Indicator>
+                  </div>
+
+                  <span className='font-semibold text-xl text-white leading-tight 
+                          group-data-[state=checked]:line-through
+                        group-data-[state=checked]:text-zinc-400'//quando checked, texto fica rasurado.
+                  >
+                     {habit.title}
+                  </span>
+               </Checkbox.Root>
+            )
+         })}
+
+      </div>
+   )
+}
