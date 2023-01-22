f160823ef4a61e19e94c45d93628cfbe47475358
André Alves Correia <andre2013aguabranca@gmail.com>
2023-01-21 23:34:40 -0300

    usage of host ip inside .env
    I made this so it's possible to connect the mobile side (with expo app on physical phone) with the server.

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,7 @@ import Fastify from 'fastify'
 
 import cors from '@fastify/cors'
 import { appRoutes } from './routes'
+import { env } from 'node:process'
 
 const app = Fastify()
 app.register(cors)
@@ -12,6 +13,7 @@ app.register(appRoutes)
 
 app.listen({
     port: 3333,
+    host: env.HOST_IP
 }).then(()=>{
-    console.log('servidor rodando de boa')
+    console.log('servidor rodando de boa no localhost:3333')
 })
