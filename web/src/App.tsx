8f75c6f05cfdfb28ae0a016a38388b8efb9b71ae
André Alves Correia <58227057+andrealvescorreia@users.noreply.github.com>
2023-01-27 12:42:02 -0300

    conecting front with back: push notifications

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,18 +2,42 @@ import './styles/global.css'
 import './lib/dayjs'
 import { Header } from './components/Header'
 import { SummaryTable } from './components/SummaryTable'
+import { api } from './lib/axios'
 
-navigator.serviceWorker.register('service-worker.js')
+navigator.serviceWorker.register('service-worker.js')// como o 'register' é uma promise, podemos usar o 'then'
+   .then(async serviceWorker => {
+      // subscription: é a assinatura do usuario que quer receber notificações
+      // pushManager é a API experimental
+      let subscription = await serviceWorker.pushManager.getSubscription()
+
+      if(!subscription){// se ele ainda não tem uma inscrição...
+         const publicKeyResponse =await api.get('/push/public_key')
+         
+         // .. cria uma nova!
+         subscription = await serviceWorker.pushManager.subscribe({
+            // public key lá do back:
+            applicationServerKey: publicKeyResponse.data.publicKey,
+            userVisibleOnly: true
+         })
+      }
+      await api.post('/push/register', {
+         subscription,
+      })
+
+      await api.post('/push/send', {
+         subscription,
+      })
+   })
 
 export function App() {
-  return (
-    <div className='w-screen h-screen flex justify-center items-center'>
-      <div className='w-full max-w-5xl px-6 flex flex-col gap-16' > 
-        <Header />
-        <SummaryTable />
+   return (
+      <div className='w-screen h-screen flex justify-center items-center'>
+         <div className='w-full max-w-5xl px-6 flex flex-col gap-16' >
+            <Header />
+            <SummaryTable />
+         </div>
       </div>
-    </div>
-  )
+   )
 }
 
 
