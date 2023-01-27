75501b7b65030251a2a18511ae6ef0618ab7bf1d
André Alves Correia <58227057+andrealvescorreia@users.noreply.github.com>
2023-01-27 12:41:07 -0300

    removed comment

--- web/public/service-worker.js
+++ web/public/service-worker.js
@@ -1,5 +1,4 @@
 // service workers são scripts que ficam rodando de background no browser mesmo após o app ser fechado.
-//console.log('o hey brothers')
 
 self.addEventListener('push', function (event){
    const body =event.data?.text() ?? ''
